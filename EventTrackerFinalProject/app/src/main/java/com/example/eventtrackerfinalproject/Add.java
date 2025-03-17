ee7270c07f8865f1965e07e49cdd714f872845c7
Mitchell <105067754+MitchellFont@users.noreply.github.com>
2025-03-17 17:09:44 -0400

    Update Add.java

--- EventTrackerFinalProject/app/src/main/java/com/example/eventtrackerfinalproject/Add.java
+++ EventTrackerFinalProject/app/src/main/java/com/example/eventtrackerfinalproject/Add.java
@@ -1,240 +1,138 @@
 package com.example.eventtrackerfinalproject;
 
-import android.Manifest;
-import android.annotation.SuppressLint;
-import android.app.AlarmManager;
-import android.app.AlertDialog;
 import android.app.DatePickerDialog;
-import android.app.PendingIntent;
 import android.app.TimePickerDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.pm.PackageManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-import java.text.DateFormat;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 
 public class Add extends AppCompatActivity {
-
-    Button addB, timeB, dateB, requestB;
-    String alarm;
-    private final int SMS_PERMISSION_CODE = 1;
-
-    EditText titleIN, descriptionIN;
-
-    long notificationC;
+    private Button addButton, timeButton, dateButton, requestButton;
+    private EditText titleInput, descriptionInput;
+    private ReminderManager reminderManager;
+    private PermissionManager permissionManager;
+    private MainDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
 
-        // Initialize buttons and EditTexts
-        requestB = findViewById(R.id.request_button);
-        titleIN = findViewById(R.id.title_input);
-        descriptionIN = findViewById(R.id.description_input);
-        timeB = findViewById(R.id.btnTime);
-        dateB = findViewById(R.id.btnDate);
-        addB = findViewById(R.id.add_new);
+        // Initialize components
+        initViews();
+        reminderManager = new ReminderManager(this);
+        permissionManager = new PermissionManager(this);
+        database = new MainDatabase(this);
 
-        // Time and date input listeners
-        dateB.setOnClickListener(view -> selectDate());
-        timeB.setOnClickListener(view -> selectTime());
-
-        // Add text watchers to enable/disable the add button
-        titleIN.addTextChangedListener(textWatcher);
-        descriptionIN.addTextChangedListener(textWatcher);
-        timeB.addTextChangedListener(textWatcher);
-        dateB.addTextChangedListener(textWatcher);
-
-        // Add event button listener
-        addB.setOnClickListener(view -> {
-            MainDatabase db = new MainDatabase(Add.this);
-
-            requestPermission();
-
-            notificationC = db.addReminder(
-                    titleIN.getText().toString().trim(),
-                    dateB.getText().toString().trim(),
-                    descriptionIN.getText().toString().trim(),
-                    timeB.getText().toString().trim()
-            );
-
-            // Set the alarm if permission is granted
-            if (ContextCompat.checkSelfPermission(Add.this,
-                    Manifest.permission.READ_SMS) == PackageManager.PERMISSION_GRANTED) {
-                try {
-                    setAlarm(
-                            titleIN.getText().toString().trim(),
-                            dateB.getText().toString().trim(),
-                            descriptionIN.getText().toString().trim(),
-                            timeB.getText().toString().trim()
-                    );
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                }
-            } else {
-                Toast.makeText(Add.this, "For this feature you need to give permission", Toast.LENGTH_SHORT).show();
-            }
-
-            finish();
-        });
-
-        // Request permission button listener
-        requestB.setOnClickListener(view -> {
-            if (ContextCompat.checkSelfPermission(Add.this,
-                    Manifest.permission.READ_SMS) == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(Add.this, "You gave permission already", Toast.LENGTH_SHORT).show();
-            } else {
-                requestPermission();
-            }
-        });
+        // Set listeners
+        setListeners();
     }
 
-    // Set alarm
-    private void setAlarm(String date, String time, String text, String description) throws ParseException {
-        Intent intent = new Intent(Add.this, Alarm.class);
-        intent.putExtra("time", date);
-        intent.putExtra("date", time);
-        intent.putExtra("event", text);
-        intent.putExtra("description", description);
+    private void initViews() {
+        requestButton = findViewById(R.id.request_button);
+        titleInput = findViewById(R.id.title_input);
+        descriptionInput = findViewById(R.id.description_input);
+        timeButton = findViewById(R.id.btnTime);
+        dateButton = findViewById(R.id.btnDate);
+        addButton = findViewById(R.id.add_new);
+    }
 
-        PendingIntent pending = PendingIntent.getBroadcast(
-                Add.this,
-                (int)notificationC,
-                intent,
-                PendingIntent.FLAG_UPDATE_CURRENT);
+    private void setListeners() {
+        dateButton.setOnClickListener(view -> selectDate());
+        timeButton.setOnClickListener(view -> selectTime());
 
-        AlarmManager alarmM = (AlarmManager) getSystemService(ALARM_SERVICE);
-        String dateTime = date + " " + alarm;
-        DateFormat format = new SimpleDateFormat("d-M-yyyy hh:mm");
-        try {
-            Date dateToSet = format.parse(dateTime);
-            assert dateToSet != null;
+        titleInput.addTextChangedListener(textWatcher);
+        descriptionInput.addTextChangedListener(textWatcher);
 
-            alarmM.set(AlarmManager.RTC_WAKEUP,
-                    dateToSet.getTime(),
-                    pending);
-            Toast.makeText(getApplicationContext(), "Alarm set", Toast.LENGTH_SHORT).show();
+        addButton.setOnClickListener(view -> addReminder());
+        requestButton.setOnClickListener(view -> permissionManager.requestSmsPermission());
+    }
 
-        } catch (ParseException e) {
-            e.printStackTrace();
+    private void addReminder() {
+        Reminder reminder = new Reminder(
+                titleInput.getText().toString().trim(),
+                dateButton.getText().toString().trim(),
+                descriptionInput.getText().toString().trim(),
+                timeButton.getText().toString().trim()
+        );
+
+        long notificationId = database.addReminder(reminder);
+
+        if (permissionManager.hasSmsPermission()) {
+            try {
+                reminderManager.setAlarm(reminder);
+            } catch (ParseException e) {
+                Toast.makeText(this, "Failed to set alarm", Toast.LENGTH_SHORT).show();
+                e.printStackTrace();
+            }
+        } else {
+            Toast.makeText(this, "Permission required for this feature", Toast.LENGTH_SHORT).show();
         }
-    }
 
-    // Date and time set
-    private void selectTime() {
-        Calendar cal = Calendar.getInstance();
-        int hr = cal.get(Calendar.HOUR_OF_DAY);
-        int min = cal.get(Calendar.MINUTE);
-        TimePickerDialog timePickerDialog = new TimePickerDialog(this, (timePicker, i, i1) -> {
-            alarm = i + ":" + i1;
-            timeB.setText(formatTime(i, i1));
-        }, hr, min, false);
-        timePickerDialog.show();
+        finish();
     }
 
     private void selectDate() {
-        Calendar cal = Calendar.getInstance();
-        int Y = cal.get(Calendar.YEAR);
-        int M = cal.get(Calendar.MONTH);
-        int D = cal.get(Calendar.DAY_OF_MONTH);
-        @SuppressLint("DefaultLocale") DatePickerDialog datePickerDialog = new DatePickerDialog(
-                this, (datePicker, year, month, day) ->
-                dateB.setText(String.format(
-                        "%d-%d-%d", day, month + 1, year)),
-                Y, M, D);
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                this,
+                (view, selectedYear, selectedMonth, selectedDay) -> dateButton.setText(String.format("%d-%d-%d", selectedDay, selectedMonth + 1, selectedYear)),
+                year, month, day);
         datePickerDialog.show();
     }
 
-    public String formatTime(int hr, int min) {
-        String T;
-        String RM;
+    private void selectTime() {
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
 
-        if (min / 10 == 0) {
-            RM = "0" + min;
-        } else {
-            RM = "" + min;
-        }
-
-        if (hr == 0) {
-            T = "12" + ":" + RM + " AM";
-        } else if (hr < 12) {
-            T = hr + ":" + RM + " AM";
-        } else if (hr == 12) {
-            T = "12" + ":" + RM + " PM";
-        } else {
-            int temp = hr - 12;
-            T = temp + ":" + RM + " PM";
-        }
-        return T;
+        TimePickerDialog timePickerDialog = new TimePickerDialog(
+                this,
+                (view, selectedHour, selectedMinute) -> {
+                    String time = formatTime(selectedHour, selectedMinute);
+                    timeButton.setText(time);
+                },
+                hour, minute, false);
+        timePickerDialog.show();
     }
 
-    // TextWatcher to check if all required fields are filled
+    private String formatTime(int hour, int minute) {
+        String formattedMinute = (minute < 10) ? "0" + minute : String.valueOf(minute);
+        String period = (hour < 12) ? "AM" : "PM";
+        int formattedHour = (hour == 0 || hour == 12) ? 12 : hour % 12;
+        return String.format("%d:%s %s", formattedHour, formattedMinute, period);
+    }
+
     private final TextWatcher textWatcher = new TextWatcher() {
         @Override
         public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
 
         @Override
         public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            String title = titleIN.getText().toString().trim();
-            String description = descriptionIN.getText().toString().trim();
-            String date = dateB.getText().toString().trim();
-            String time = timeB.getText().toString().trim();
+            String title = titleInput.getText().toString().trim();
+            String description = descriptionInput.getText().toString().trim();
+            String date = dateButton.getText().toString().trim();
+            String time = timeButton.getText().toString().trim();
 
-            addB.setEnabled(!title.isEmpty() && !description.isEmpty() &&
-                    !date.isEmpty() && !time.isEmpty());
+            addButton.setEnabled(!title.isEmpty() && !description.isEmpty() && !date.isEmpty() && !time.isEmpty());
         }
 
         @Override
         public void afterTextChanged(Editable editable) {}
     };
-
-    // Request SMS permission
-    private void requestPermission() {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_SMS)) {
-            new AlertDialog.Builder(this)
-                    .setTitle("SMS messages")
-                    .setMessage("You need to give permissions for reminders")
-                    .setPositiveButton("OK", (dialogInterface, i) -> ActivityCompat.requestPermissions(
-                            Add.this,
-                            new String[]{Manifest.permission.READ_SMS},
-                            SMS_PERMISSION_CODE))
-                    .setNegativeButton("Cancel", (dialogInterface, i) -> dialogInterface.dismiss())
-                    .create().show();
-        } else {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.READ_SMS},
-                    SMS_PERMISSION_CODE);
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int request,
-                                           @NonNull String[] permissions,
-                                           @NonNull int[] grant) {
-        super.onRequestPermissionsResult(request, permissions, grant);
-        if (request == SMS_PERMISSION_CODE) {
-            if (grant.length > 0 && grant[0] == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
 }
