6fb4858b62d46fd8fda4475b77d2f20eeabed17a
Mitchell <105067754+MitchellFont@users.noreply.github.com>
2025-03-17 17:10:39 -0400

    Create Constants.java

---
+++ EventTrackerFinalProject/app/src/main/java/com/example/eventtrackerfinalproject/Constants.java
@@ -0,0 +1,7 @@
+public class Constants {
+    public static final String EXTRA_TIME = "time";
+    public static final String EXTRA_DATE = "date";
+    public static final String EXTRA_EVENT = "event";
+    public static final String EXTRA_DESCRIPTION = "description";
+    public static final int SMS_PERMISSION_CODE = 1;
+}
