9d9b2623cae95c6e0eaf04f40b53f6c4f29bf2e9
Mitchell <105067754+MitchellFont@users.noreply.github.com>
2025-03-17 17:14:14 -0400

    Update Constants.java
    Add constants

--- EventTrackerFinalProject/app/src/main/java/com/example/eventtrackerfinalproject/Constants.java
+++ EventTrackerFinalProject/app/src/main/java/com/example/eventtrackerfinalproject/Constants.java
@@ -4,4 +4,6 @@ public class Constants {
     public static final String EXTRA_EVENT = "event";
     public static final String EXTRA_DESCRIPTION = "description";
     public static final int SMS_PERMISSION_CODE = 1;
+    public static final String NOTIFICATION_CHANNEL_ID = "notifyText";
+    public static final int NOTIFICATION_ID = 200;
 }
