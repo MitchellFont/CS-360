0614c25c47d885452a5d0bccf991cabc51335ab2
Mitchell <105067754+MitchellFont@users.noreply.github.com>
2025-03-24 12:56:45 -0400

    Update ReminderManager.java to add Priority queue

--- EventTrackerFinalProject/app/src/main/java/com/example/eventtrackerfinalproject/ReminderManager.java
+++ EventTrackerFinalProject/app/src/main/java/com/example/eventtrackerfinalproject/ReminderManager.java
@@ -1,19 +1,44 @@
+package com.example.eventtrackerfinalproject;
+
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.widget.Toast;
 
-import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class ReminderManager {
     private Context context;
+    private Queue<Reminder> reminderQueue;
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm");
 
     public ReminderManager(Context context) {
         this.context = context;
+        this.reminderQueue = new PriorityQueue<>((r1, r2) -> {
+            try {
+                Date date1 = dateFormat.parse(r1.getDate() + " " + r1.getTime());
+                Date date2 = dateFormat.parse(r2.getDate() + " " + r2.getTime());
+                return date1.compareTo(date2);
+            } catch (ParseException e) {
+                return 0;
+            }
+        });
+    }
+
+    public void addToQueue(Reminder reminder) {
+        reminderQueue.add(reminder);
+    }
+
+    public void processNextReminder() throws ParseException {
+        if (!reminderQueue.isEmpty()) {
+            Reminder next = reminderQueue.poll();
+            setAlarm(next);
+        }
     }
 
     public void setAlarm(Reminder reminder) throws ParseException {
