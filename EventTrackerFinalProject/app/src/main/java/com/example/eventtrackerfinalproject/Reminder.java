7ab86c3c25f0ba27688efd27d6e7f3eadd155138
Mitchell <105067754+MitchellFont@users.noreply.github.com>
2025-04-04 10:43:20 -0400

    Update Reminder.java, Add ID

--- EventTrackerFinalProject/app/src/main/java/com/example/eventtrackerfinalproject/Reminder.java
+++ EventTrackerFinalProject/app/src/main/java/com/example/eventtrackerfinalproject/Reminder.java
@@ -1,17 +1,29 @@
+package com.example.eventtrackerfinalproject;
+
 public class Reminder {
+    private String id;
     private String title;
     private String date;
     private String description;
     private String time;
 
-    public Reminder(String title, String date, String description, String time) {
+    public Reminder(String id, String title, String date, String description, String time) {
+        this.id = id;
         this.title = title;
         this.date = date;
         this.description = description;
         this.time = time;
     }
 
+    public Reminder(String title, String date, String description, String time) {
+        this("", title, date, description, time);
+    }
+
     // Getters and setters
+    public String getId() {
+        return id;
+    }
+    
     public String getTitle() {
         return title;
     }
@@ -26,5 +38,9 @@ public class Reminder {
 
     public String getTime() {
         return time;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 }
